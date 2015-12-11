cd405647012520bdaa190a374c4e502c542646de
shubhanshu-gupta <shubhanshu2007@gmail.com>
2015-12-11 12:28:27 +0530

    Basic template, index views and routing of apt URL working

--- imdb/imdbsearch/views.py
+++ imdb/imdbsearch/views.py
@@ -3,20 +3,32 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse
 from jinja2 import Template
 from .forms import NameForm
+import jinja2
+from jinja2.ext import loopcontrols
+jinja_environ = jinja2.Environment(loader=jinja2.FileSystemLoader(['ui']), extensions=[loopcontrols])
 
 # def index(request):
 # 	template = Template('imdbsearch/url/to/submit/')
 # 	return HttpResponse(template.render('index.html'))
 
 def index(request):
+	form = NameForm()
+	template = loader.get_template('imdbsearch/index.html')
+	Context = RequestContext(request, {'form': form})
+	return HttpResponse(template.render(Context))
 
-	if request.method=='POST':
-		#creating a form instance and populating with data from the request
-		form = NameForm(request.POST)
-		if form.is_valid():
-			return HttpResponse('/result/')
+	# query = request.GET['q']
+	# t = loader.get_template('imdbsearch/index.html')
+	# c = Context({ 'query' : query,})
+	# return HttpResponse(t.render(c))
 
-	else:
-		form = NameForm()
+	# if request.method=='POST':
+	# 	#creating a form instance and populating with data from the request
+	# 	form = NameForm(request.POST)
+	# 	if form.is_valid():
+	# 		return HttpResponse('/result/')
 
-	return render(request, 'index.html', {'form': form})
+	# else:
+	# 	form = NameForm()
+
+	# return render(request, 'index.html', {'form': form})
